a85098d10ba77dcb5c19e5fab1e0ac68c96d90f9
user <user@MacBook-Pro-de-user.local>
2022-02-04 18:22:38 -0300

    componente cajas metricas props

--- src/components/ContentRowTop.js
+++ src/components/ContentRowTop.js
@@ -3,28 +3,27 @@ import ContentRowMovies from "./ContentRowMovies";
 import GenresIndDb from "./GenresInDb";
 import LastMovieInDb from "./LastMovieInDb";
 
-function ContentRowTop (){
+function ContentRowTop() {
     return (
         <React.Fragment>
             {/*<!-- Content Row Top -->*/}
-			<div className="container-fluid">
-				<div className="d-sm-flex align-items-center justify-content-between mb-4">
-					<h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
-				</div>
+            <div className="container-fluid">
+                <div className="d-sm-flex align-items-center justify-content-between mb-4">
+                    <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
+                </div>
 
                 {/*<!-- Content Row Movies-->*/}
-				<div className="row">
-                    <ContentRowMovies/>
-				
-				    {/*<!-- Content Row Last Movie in Data Base -->*/}
-				    <div className="row">
-					    <LastMovieInDb/>
-                        <GenresIndDb/>
-                    </div>
+                <div className="row">
+                    <ContentRowMovies />
 
-			    </div>
+                    {/*<!-- Content Row Last Movie in Data Base -->*/}
+                    <div className="row">
+                        <LastMovieInDb />
+                        <GenresIndDb />
+                    </div>
+                </div>
             </div>
-			{/*<!--End Content Row Top-->*/}
+            {/*<!--End Content Row Top-->*/}
         </React.Fragment>
     )
 }
